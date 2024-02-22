de400533a4714839ef7f6cf67beb6d82f57a4897
AndreyZ <andreyzv2525@gmail.com>
2024-02-22 17:37:04 +0300

    adad

--- testModel/app.py
+++ testModel/app.py
@@ -1,14 +1,24 @@
 # объявление библиотек
-import cv2 as cv
-import numba as nb
 import numpy as np
+import cv2
+import subprocess as sp
+import multiprocessing as mp
+from os import remove
 
-# путь до img
-path_to_test = "test.jpg"
 
-img = cv.imread(path_to_test)
-cv.imshow("test_leable", img)
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while(True):
+  ret, frame = cap.read()
+  frame = cv2.rectangle(frame,(384,0),(510,128),(0,0,255),10)
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  gray = cv2.GaussianBlur(gray, (7, 7), 1.5) # Параметры позволяют регулировать шумность
 
-key = cv.waitKey(100)
-if key == key == 27: # Esc
-    cv.destroyAllWindows()
+  edges = cv2.Canny(gray, 1, 50) # Нахождение контуров
+  cv2.imshow("edges",edges) # вывод  на экран # оригинальный вариант
+
+
+cap.release()
+cv2.destroyAllWindows()
