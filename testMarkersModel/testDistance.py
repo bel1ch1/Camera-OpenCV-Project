61071de4bf369b9f2766da4fa2b2db54d6dc5a8a
AndreyZ <andreyzv2525@gmail.com>
2024-03-18 17:04:10 +0300

    detec aruco markers

--- testMarkersModel/testDistance.py
+++ testMarkersModel/testDistance.py
@@ -1,17 +1,25 @@
 import cv2
+import pickle
+
+
+# Подгружаем данные для определения дистанции
+with open('dist.pkl', 'rb') as f:
+    dist = pickle.load(f)
+
+with open('cameraMatrix.pkl', 'rb') as g:
+    mtx = pickle.load(g)
+
+
 
 # Метод выполнят поиск маркеров и рисует контуры
 def findArucoMarkers(img, draw=True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
     arucoParam = cv2.aruco.DetectorParameters()
-
-    markerSizeCM = 16.5
-    rvec , tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeCM, mtx, dist)
-    corners, ids, rejected = cv2.aruco.detectMarkers(gray, arucoDict, parameters = arucoParam)
+    (corners, ids, rejected) = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
     if draw:
         cv2.aruco.drawDetectedMarkers(img, corners)
-    return [corners, tvec]
+    return [corners]
 
 
 # захват видео с камеры и обработка
@@ -22,8 +30,8 @@ while True:
     success, img = cap.read()
     arucofound = findArucoMarkers(img)
     if len(arucofound[0])!=0:
-        for corner, tvec in zip(arucofound[0], arucofound[1]):
-            print(corner[0][0], corner[0][1], corner[0][2], corner[0][3], tvec[2]/100)
+        for corner in arucofound[0]:
+            print(corner[0][0], corner[0][1], corner[0][2], corner[0][3])
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
