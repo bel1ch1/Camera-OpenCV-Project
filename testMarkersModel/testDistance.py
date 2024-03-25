10e6a50c5690f1f707e6b84f9442c963ccffa63a
AndreyZ <andreyzv2525@gmail.com>
2024-03-25 16:36:10 +0300

    displaying information on a web page

--- testMarkersModel/testDistance.py
+++ testMarkersModel/testDistance.py
@@ -1,7 +1,7 @@
 import cv2
 import pickle
 import numpy as np
-
+import streamlit as st
 
 # Подгружаем данные для определения дистанции
 with open('dist.pkl', 'rb') as f:
@@ -23,7 +23,7 @@ arucoParam = cv2.aruco.DetectorParameters()
 def pose_esitmation(img, arucoDict):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Ищем маркеры
-    corners, ids, _ = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
+    corners, _, _ = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParam)
     # если нашли
     if corners:
         # Определяем сдвиг
@@ -33,26 +33,44 @@ def pose_esitmation(img, arucoDict):
                         tvec ** 2 + tvec ** 2 + tvec ** 2
                     )
         # Вывод в консоль
-        for corner in corners:
-            print(corner[0][0], corner[0][1], corner[0][2], corner[0][3])
-        print(distance_to_marker)
-    return 0
+        c1 = corners[0][0][0]
+        c2 = corners[0][0][1]
+        c3 = corners[0][0][2]
+        c4 = corners[0][0][3]
+        #print(c1[:1], c2[:1], c3[:1], c4[:1])
+        d = distance_to_marker[0][0]
+
+        print(c1, c2, c3, c4, d[:1])
+    #return [c1[:1], c2[:1], c3[:1], c4[:1]]
+        return f"1 {c1[:1]},\n2 {c2[:1]},\n3 {c3[:1]},\n4 {c4[:1]},\ndistance {d[:1]}"
+
+
+st.title("corners coords distasnce")
+pose_estmition_data_area = st.empty()
+frame_placeholder = st.empty()
+stop_btn_pressed = st.button("Stop")
+start_btn_pressed = st.button("Start")
 
 
 # захват видео с камеры и обработка
-cap = cv2.VideoCapture(0)
-while True:
-    success, frame = cap.read()
-    if not success:
-        continue
+if start_btn_pressed:
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened() and not stop_btn_pressed:
 
-    pose_esitmation(frame, ARUCO_DICT)
+        success, frame = cap.read()
+        if not success:
+            break
 
-    cv2.imshow("distance", frame)
+        #pose_esitmation(frame, ARUCO_DICT)
+        frame_placeholder.image(frame, channels="BGR")
+        output=pose_esitmation(frame, ARUCO_DICT)
+        pose_estmition_data_area.text(f"data\n{output}")
+        #cv2.imshow("frame", frame)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        k = cv2.waitKey(30) & 0xff
+        if k == 27 or stop_btn_pressed:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+
+    cap.release()
+    cv2.destroyAllWindows()
